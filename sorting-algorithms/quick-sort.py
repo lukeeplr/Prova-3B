cf0268be93a860df187b7ad311cf057e5ea8ca0d
Lucas <53486649+lukeeplr@users.noreply.github.com>
2019-10-23 11:19:44 -0300

    Create quick-sort.py

---
+++ sorting-algorithms/quick-sort.py
@@ -0,0 +1,48 @@
+def quicksort(array):
+    tam = len(array)
+    if tam < 2:
+        return array
+    from random import randint
+    menor, igual, maior = [], [], []
+    pivot = array[randint(0, tam-1)]
+    for x in array:
+        if x < pivot: menor.append(x)
+        elif x > pivot: maior.append(x)
+        else: igual.append(x)
+    return (quicksort(menor) + igual + quicksort(maior))
+
+
+def getpivot(arr, c, f):
+        m = (c + f) // 2
+        pivot = f
+        if (arr[c] < arr[m]):
+            if (arr[c] < arr[f]):
+                pivot = m
+        elif (arr[c] < arr[f]):
+                pivot = c
+        return pivot
+
+
+def partition(arr, c, f):
+    p = getpivot(arr, c, f)
+    arr[p], arr[f] = arr[f], arr[p]
+    pivot = arr[f]
+    x = c - 1
+    for y in range(c,f):
+        if arr[y] < pivot:
+            x += 1
+            arr[x],arr[y] = arr[y], arr[x]
+    arr[x + 1], arr[f] = arr[f], arr[x + 1]
+    return (x+1)
+
+
+def quick(arr, c, f):
+    if c < f:
+        p = partition(arr,c,f)
+        quick(arr, c, p - 1)
+        quick(arr,p + 1, f)
+    return arr
+
+
+def quicker(arr):
+    return quick(arr, 0, len(arr)-1)
