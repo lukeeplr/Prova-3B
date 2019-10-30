c537b4dbfed32cc61e9c66d9231a6b9525db4c8f
Lucas <53486649+lukeeplr@users.noreply.github.com>
2019-10-30 04:09:29 -0300

    Update sequential-search.py

--- search-algorithms/sequential-search.py
+++ search-algorithms/sequential-search.py
@@ -3,8 +3,6 @@ def sequential_search(arr, key):
     found = False
     x = 0
     while (x <= n) and (not found):
-        if arr[x] == key:
-            found = True
-        else:
-            x += 1
+        if (arr[x] == key):   found = True
+        else:   x += 1
     return found
