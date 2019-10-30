fb954007b62ca9a5349c38f66d702f78ab68a530
Lucas <53486649+lukeeplr@users.noreply.github.com>
2019-10-30 03:46:43 -0300

    Update binary-search.py

--- search-algorithms/binary-search.py
+++ search-algorithms/binary-search.py
@@ -1,11 +1,8 @@
 def binary_search(arr, key):
-    mid = (len(arr) // 2)
-    if key == arr[mid]:
-        return True
-    elif mid == 1:
-        return False
+    tam = len(list(arr))
+    mid = (tam // 2)
+    if (arr[mid] == key):   return True
+    elif (tam == 1):    return False
     else:
-        if key < arr[mid]:
-            return binary_search(arr[:mid], key)
-        else:
-            return binary_search(arr[(mid +1):], key)
+        if (key < arr[mid]):    return binary_search(arr[:mid], key)
+        else:   return binary_search(arr[(mid + 1):], key)
