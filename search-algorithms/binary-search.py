3d74a34f16afc32920fb4dc95a86f22e98bc8a9c
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-12-13 00:28:01 -0300

    Update binary-search.py

--- search-algorithms/binary-search.py
+++ search-algorithms/binary-search.py
@@ -1,8 +1,30 @@
-def binary_search(arr, key):
-    tam = len(list(arr))
+def binary_search_recursive(arr, key):
+    tam = len(arr)
     mid = (tam // 2)
     if (arr[mid] == key):   return True
     elif (tam == 1):    return False
     else:
         if (key < arr[mid]):    return binary_search(arr[:mid], key)
         else:   return binary_search(arr[(mid + 1):], key)
+
+
+
+def binary_search(arr, key):
+    start = 0
+    end = len(arr) - 1
+    mid = (start + end // 2)
+
+    while ((arr[mid] != key) and (start <= end)):
+        
+        if (key < arr[mid]):
+            end = mid - 1
+        
+        else:
+            start = mid + 1
+
+        mid = ((start + end) // 2)
+
+    if (arr[mid] == key):
+        return True
+    else:
+        return False
