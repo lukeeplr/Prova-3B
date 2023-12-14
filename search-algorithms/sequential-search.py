8f3346018111e28047335a34d5d6733bae4c4034
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-12-14 20:19:38 -0300

    feat: adds recursive sequential search algorithm

--- search-algorithms/sequential-search.py
+++ search-algorithms/sequential-search.py
@@ -6,3 +6,15 @@ def sequential_search(arr, key):
         if (arr[x] == key):   found = True
         else:   x += 1
     return found
+
+
+def sequencial_recursiva(lista, chave, indice = 0):
+
+    if (indice == len(lista)):
+        return False
+    
+    elif (chave == lista[indice]):
+        return True
+    
+    else:
+        return sequencial_recursiva(lista, chave, indice + 1)
